597bed519bafc91c5d20f6eaf8a23494e47399e5
Wahab Hussain <wahabh@humanforce.com>
2024-11-15 23:02:07 +0500

    Addition of components for the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LeftSidebarComponent } from "./left-sidebar/left-sidebar.component";
+import { FeedComponent } from "./feed/feed.component";
+import { RightSidebarComponent } from "./right-sidebar/right-sidebar.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LeftSidebarComponent, FeedComponent, RightSidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
